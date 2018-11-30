ce7338ef4aa550c737139a155849b3f81bf25e3b
jlevym <jlevym@gmail.com>
2018-11-30 14:32:16 +0200

    implement ShoppingListService

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,17 +1,19 @@
 import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
+
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @Output() newListItem = new EventEmitter<Ingredient>();
+
   @ViewChild('nameInput') nameRef: ElementRef;
   @ViewChild('amountInput') amountRef: ElementRef;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -20,7 +22,7 @@ export class ShoppingEditComponent implements OnInit {
        const name = this.nameRef.nativeElement.value;
        const amount = this.amountRef.nativeElement.value;
        const newIngredient = new Ingredient(name, amount);
-       this.newListItem.emit(newIngredient);
+       this.shoppingListService.newListItem.emit(newIngredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  newListItem = new EventEmitter<Ingredient>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  pushIngredients(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
