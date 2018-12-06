3331ccc462523ecce1f2617de0262ac5bae15b53
jlevym <jlevym@gmail.com>
2018-12-06 18:09:10 +0200

    replace ngIf on tabs for routing with routerLink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,27 +1,9 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  selection = 'recipe';
-  @Output() showRecipeChanged = new EventEmitter<string>();
-
-  constructor(private shoppingListService: ShoppingListService) { }
-
-  ngOnInit() {
-    this.shoppingListService.changeViewToShoppingList.subscribe(
-      () => {
-        this.showRecipeChanged.emit('shopping-list');
-      }
-    );
-  }
-
-  toSelector(selector: string) {
-    this.selection = selector;
-    this.showRecipeChanged.emit(this.selection);
-  }
+export class HeaderComponent {
 }
