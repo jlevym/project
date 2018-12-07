08686f817666289fae42022b46ebdbcb1a9fee4c
jlevym <jlevym@gmail.com>
2018-12-07 10:51:06 +0200

    extracting id and editMode for edit and new recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {NgModule} from '@angular/core';
 import {RecipeDetailsComponent} from './recipe/recipe-details/recipe-details.component';
 import {RecipeStartComponent} from './recipe/recipe-start/recipe-start.component';
+import {RecipeEditComponent} from './recipe/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
   { path: 'recipe', component: RecipeComponent, children: [
       {path: '', component: RecipeStartComponent},
-      {path: ':id', component: RecipeDetailsComponent}
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailsComponent},
+      {path: ':id/edit', component: RecipeEditComponent}
     ] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
