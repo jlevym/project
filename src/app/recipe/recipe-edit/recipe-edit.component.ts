2ba7c5cdb3116ec3f9ceb961550ee38933cb2a9a
jlevym <jlevym@gmail.com>
2018-12-14 12:14:43 +0200

    refactor code accoding to lecture, and add arrays which is more complicated

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 import {RecipeService} from './../../recipe/recipe.service';
-import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -14,7 +13,6 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
   id: number;
   editMode = false;
-  currentRecipe: Recipe;
 
   constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
@@ -30,30 +28,58 @@ export class RecipeEditComponent implements OnInit {
       });
   }
   private initForm() {
-    if(!this.editMode) {
-      this.recipeForm = new FormGroup({
-        'name': new FormControl('', Validators.required),
-        'imagePath': new FormControl(''),
-        'description': new FormControl('')
-      });
-    } else {
-      // get the recipe
-      this.currentRecipe = this.recipeService.getRecipe(this.id);
-      const recipeName = this.currentRecipe.name;
-      const recipePath = this.currentRecipe.imagePath;
-      const recipeDescription = this.currentRecipe.description;
+    let recipeName = '';
+    let recipePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
+    if (this.editMode) {
+      // get the clicked recipe and override the default values
+      const currentRecipe = this.recipeService.getRecipe(this.id);
+      recipeName = currentRecipe.name;
+      recipePath = currentRecipe.imagePath;
+      recipeDescription = currentRecipe.description;
+      if (currentRecipe['ingredients']) {
+        for (let ingredient of currentRecipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount': new FormControl(ingredient.amount, [Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
+            })
+          );
+        }
+      }
+    }
       this.recipeForm = new FormGroup({
         'name': new FormControl(recipeName, Validators.required),
         'imagePath': new FormControl(recipePath, Validators.required),
-        'description': new FormControl(recipeDescription)
-
+        'description': new FormControl(recipeDescription, Validators.required),
+        'ingredients': recipeIngredients
       });
-    }
   }
 
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null,
+          [Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
+      })
+    );
+  }
+
+
+
   onSave() {
-    if(!this.editMode) {
+    if (!this.editMode) {
       this.recipeService.addRecipe(this.recipeForm);
   } else {
       this.recipeService.updateRecipe(this.id, this.recipeForm);
