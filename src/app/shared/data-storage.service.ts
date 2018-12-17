d3bfb9ddc4dcd19b9a6bef3f75f1f56d70509c91
jlevym <jlevym@gmail.com>
2018-12-17 20:50:24 +0200

    implement loop to add empty array to recipes that dont have ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {Recipe} from '../recipe/recipe.model';
+import {Http, Response} from '@angular/http';
+import {map} from 'rxjs/operators';
 
 import {RecipeService} from '../recipe/recipe.service';
+import {Recipe} from '../recipe/recipe.model';
+
 
 
 @Injectable({
@@ -18,9 +20,20 @@ export class DataStorageService {
   }
   fetchRecipes() {
     this.http.get('https://angular7-udemy-project.firebaseio.com/recipes.json')
+    // implement pipe(map to check that each recipe has ingredietns.....
+      .pipe(map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+         for (const recipe of recipes) {
+           if (!recipe['ingredients']) {
+             recipe['ingredients'] = [];
+           }
+         }
+         return recipes;
+        }
+      ))
       .subscribe(
-        (response) => {
-          const recipes = response.json();
+        (recipes: Recipe[]) =>  {
           this.recipeService.setRecipes(recipes);
         }
       );
