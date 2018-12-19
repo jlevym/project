1b2d5a35d20ca420acc53dced1b7373d559465cd
jlevym <jlevym@gmail.com>
2018-12-19 19:27:25 +0200

    adding access control on routes and redirects after signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import {NgModule} from '@angular/core';
 import {RecipeDetailsComponent} from './recipe/recipe-details/recipe-details.component';
 import {RecipeStartComponent} from './recipe/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipe/recipe-edit/recipe-edit.component';
-import {RecipeResolverService} from './recipe-resolver.service';
 import {SignupComponent} from './auth/signup/signup.component';
 import {SigninComponent} from './auth/signin/signin.component';
+import {AuthGuard} from './auth/auth-guard.service';
+
 
 
 const appRoutes: Routes = [
@@ -15,9 +16,9 @@ const appRoutes: Routes = [
   { path: 'recipe', component: RecipeComponent, children: [
      /* {path: '', component: RecipeStartComponent, resolve: {recipe: RecipeResolverService}},*/
       {path: '', component: RecipeStartComponent},
-      {path: 'new', component: RecipeEditComponent},
+      {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
       {path: ':id', component: RecipeDetailsComponent},
-      {path: ':id/edit', component: RecipeEditComponent}
+      {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]}
     ] },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'signup', component: SignupComponent },
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import {Router} from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   token: string;
+  constructor(private router: Router) { }
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
@@ -29,6 +33,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
               (token: string) => this.token = token
@@ -43,7 +48,4 @@ export class AuthService {
   isAuthenticated() {
     return this.token != null;
   }
-
-
-  constructor() { }
 }
