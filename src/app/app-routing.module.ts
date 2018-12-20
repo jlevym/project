cac89dae6342bdbc2bff393a284578ce09b6ed89
jlevym <jlevym@gmail.com>
2018-12-20 23:11:57 +0200

    implementing shopping-list-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import {RouterModule, Routes} from '@angular/router';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {NgModule} from '@angular/core';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
-  { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule} from '@angular/forms';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,13 +14,12 @@ import {AuthGuard} from './auth/auth-guard.service';
 import {RecipesModule} from './recipe/recipes.module';
 import {SharedModule} from './shared/shared.module';
 import {AuthModule} from './auth/auth.module';
+import {ShoppingListModule} from './shopping-list/shopping-list-module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +28,8 @@ import {AuthModule} from './auth/auth.module';
     HttpModule,
     RecipesModule,
     SharedModule,
-    AuthModule
+    AuthModule,
+    ShoppingListModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
